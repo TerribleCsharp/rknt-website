c5f74a9628350227fcc7dc3b491a6ae658d97776
TerribleCsharp <68471853+TerribleCsharp@users.noreply.github.com>
2022-03-19 22:32:15 +0100

    style: linting

--- src/routes/news/wtryskiwacze.svelte
+++ src/routes/news/wtryskiwacze.svelte
@@ -2,21 +2,21 @@
 	<header>
 		<h1>pierwszy test podzespołów silnika Bieli3K - wtryskiwacze</h1>
 		<h2>10 grudnia</h2>
-		<p>
-			Wczoraj rozpoczęliśmy serię testów, które będziemy musieli przeprowadzić, przed pierwszą próbą
-			statyczną naszego silnika. Na pierwszy ogień poszły wtryskiwacze - kluczowy element hydrauliki
-			silnika, który pozwala na dostarczanie utleniacza paliwa, do komory głównej silnika. Test
-			przebiegł w 100% po naszej myśli i stanowi kolejny krok do realizacji naszego celu. Krótkie
-			nagranie z całego zajścia możecie zobaczyć poniżej:
-		</p>
-		<div style="margin: auto; text-align:center;">
-			<iframe
-				src="https://drive.google.com/file/d/1Ig-FhpeFp19yq7z6YUxez1sxMep6rz7_/preview"
-				width="640"
-				height="360"
-				allow="autoplay"
-				title="video"
-			/>
-		</div>
 	</header>
+	<p>
+		Wczoraj rozpoczęliśmy serię testów, które będziemy musieli przeprowadzić, przed pierwszą próbą
+		statyczną naszego silnika. Na pierwszy ogień poszły wtryskiwacze - kluczowy element hydrauliki
+		silnika, który pozwala na dostarczanie utleniacza paliwa, do komory głównej silnika. Test
+		przebiegł w 100% po naszej myśli i stanowi kolejny krok do realizacji naszego celu. Krótkie
+		nagranie z całego zajścia możecie zobaczyć poniżej:
+	</p>
+	<div style="margin: auto; text-align:center;">
+		<iframe
+			src="https://drive.google.com/file/d/1Ig-FhpeFp19yq7z6YUxez1sxMep6rz7_/preview"
+			width="640"
+			height="360"
+			allow="autoplay"
+			title="video"
+		/>
+	</div>
 </article>
