9f2d0648a51743ae7e3b0151870263f615fec8e1
TerribleCsharp <68471853+TerribleCsharp@users.noreply.github.com>
2022-03-20 22:18:15 +0100

    feat: add hamburger menu button

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let showNav = true;
 	export let transparentNav = true;
+	export let active = false;
 	const treshold = 95;
 
 	let currentPosition;
@@ -8,6 +9,8 @@
 	let oldPosition = 0;
 	let oldDirection = true;
 	const scroll = () => {
+		active = false;
+
 		offset += currentPosition - oldPosition;
 		const direction = currentPosition - oldPosition > 0 ? true : false;
 		if (oldDirection !== direction) offset = 0;
@@ -24,6 +27,10 @@
 		if (currentPosition > 100) transparentNav = false;
 		else transparentNav = true;
 	};
+
+	const toggle = () => {
+		active = !active;
+	};
 </script>
 
 <svelte:window on:scroll={scroll} bind:scrollY={currentPosition} />
@@ -36,6 +43,20 @@
 		<a href="#about" class="nav-element">O nas</a>
 		<a href="#partners" class="nav-element">Partnerzy</a>
 		<a href="#contact" class="nav-element">Kontakt</a>
+	</div>
+	<div class="nav-menu" class:active />
+	<div class="nav-menu__button" class:active on:click={toggle}>
+		<svg width="100%" height="100%" viewBox="0 0 100 100">
+			<path
+				class="line top"
+				d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+			/>
+			<path class="line middle" d="M 20,50 H 80" />
+			<path
+				class="line bottom"
+				d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+			/>
+		</svg>
 	</div>
 </nav>
 
@@ -53,9 +74,11 @@
 		backdrop-filter: blur(10px);
 		transition: all 200ms ease-out;
 		z-index: 5;
+		overflow-x: clip;
+		// overflow: clip visible;
 
 		.nav-container {
-			width: 100%;
+			width: 90%;
 			max-width: 1200px;
 			margin: auto;
 		}
@@ -66,7 +89,57 @@
 			height: 2rem;
 			margin: 1rem 0 0;
 		}
+		.nav-menu {
+			position: absolute;
+			top: 0;
+			right: 0;
+			width: 50%;
+			//height greater than 100vh protects against animations messing with the menu
+			height: 120vh;
+			max-width: 400px;
+			background-color: aqua;
+			transform: translateX(100%);
+			transition: all 600ms ease;
+			&.active {
+				transform: none;
+			}
+		}
+		.nav-menu__button {
+			position: absolute;
+			// top: 25%;
+			// right: 2%;
+			// height: 50%;
+			// aspect-ratio: 1/1;
+			top: 25%;
+			right: 1rem;
+			width: 2rem;
+			height: 2rem;
+			// background-color: #00aaaa;
 
+			.line {
+				fill: none;
+				stroke: white;
+				stroke-width: 6;
+
+				transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1),
+					stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1), transform 600ms ease;
+			}
+			.top,
+			.bottom {
+				stroke-dasharray: 60 207;
+			}
+			.bottom {
+				transform: scale(-1, 1) translate(-100%, 0);
+			}
+		}
+		.active .top,
+		.active .bottom {
+			stroke-dasharray: 62 207;
+			stroke-dashoffset: -140;
+		}
+		.active .middle {
+			transform: scale(1.1, 1) translate(-25%, 50%) rotate(-45deg);
+		}
 		@media (min-width: 800px) {
 			height: 6rem;
 			text-align: left;
@@ -111,6 +184,13 @@
 			}
 			img {
 				margin: 1.8rem 1rem 0;
+			}
+
+			.nav-menu__button {
+				top: 1.5rem;
+				right: 2rem;
+				width: 3rem;
+				height: 3rem;
 			}
 		}
 	}
